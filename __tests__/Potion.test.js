7b76626b3fd03e262b5b068870bda93792938d90
Plain Jane <jane99hsu@gmail.com>
2020-10-05 19:05:53 -0700

    added potion test and function

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -16,6 +16,7 @@ const Potion = require('../lib/Potion.js');
 // ========== return the special expectation object and 
 // ========== use the toBe method to test the exact equality against the returned result
 
+// this test is for the health potion
 test('creates a health potion object', () => {
     // keyword new creates new objects
     // when we create a new potion, it will take the string we pass in
@@ -23,9 +24,20 @@ test('creates a health potion object', () => {
     const potion = new Potion('health');
 
     // in this case, the name property is "health"
+    // checks that the name value is health
     // and the value property is a number 
     expect(potion.name).toBe('health');
     // the expect.any() method takes a constructor as an argument
     // we're expecting that the value property is created with a number() constructor
+    expect(potion.value).toEqual(expect.any(Number));
+});
+
+// this test is for a random potion
+test('create a random potion object', () => {
+    // when we create a new potion without an argument
+    const potion = new Potion();
+
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
     expect(potion.value).toEqual(expect.any(Number));
 });
