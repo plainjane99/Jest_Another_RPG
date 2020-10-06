a7497b10326fa1aaf887c1d04bacd72c7d6b3b47
Plain Jane <jane99hsu@gmail.com>
2020-10-05 22:36:08 -0700

    added player health, attack, and potion methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -66,5 +66,86 @@ test('gets inventory from player or returns false', () => {
 });
 // ========== player inventory test code ends here ========== //
 
+// ========== player health test code goes here ========== //
+test("gets player's health value", () => {
+    const player = new Player('Dave');
+
+    // expect.stringContaining() method is an expect method
+    // that we can use to make sure our string includes our player's health
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+// ========== player health test code ends here ========== //
+
+// ========== player is alive test code goes here ========== //
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+    
+    // check that player is alive (true) condition
+    expect(player.isAlive()).toBeTruthy();
+    // updating the value of our player health halfway through the test
+    // so that we can check for false conditions
+    player.health = 0;
+    expect(player.isAlive()).toBeFalsy();
+});
+// ========== player is alive test code ends here ========== //
+
+// ========== player reduced health test code goes here ========== //
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+
+    const oldHealth = player.health;
+    // calls reduceHealth and subtracts 5
+    player.reduceHealth(5);
+    // does the math
+    expect(player.health).toBe(oldHealth - 5);
+
+    // calls reduceHealth and subtracts 99999
+    player.reduceHealth(99999);
+    // sets player.health to 0
+    expect(player.health).toBe(0);
+});
+// ========== player reduced health test code ends here ========== //
+
+// ========== player attack test code goes here ========== //
+test("gets player's attack value", () => {
+    const player = new Player('Dave');
+    // hard code player strength
+    player.strength = 10;
+
+    // provide specific numbers to test since random numbers are too general to test
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+// ========== player attack test code ends here ========== //
+
+// ========== check potion was added correctly test code goes here ========== //
+test('adds a potion to the inventory', () => {
+    const player = new Player('Dave');
+    
+    // keep track of the old count
+    const oldCount = player.inventory.length;
+    
+    // calls new Potion
+    player.addPotion(new Potion());
+
+    // confirms new length is greater than old length
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+// ========== check potion was added correctly test code ends here ========== //
+
+// ========== use potion test code goes here ========== //
+test('uses a potion from inventory', () => {
+    const player = new Player('Dave');
+    // creates an inventory with calls to potion
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    // keeps track of old count
+    const oldCount = player.inventory.length;
+    // uses a potion
+    player.usePotion(1);
+    // confirms new length is less than old length
+    expect(player.inventory.length).toBeLessThan(oldCount);
+});
+// ========== use potion test code ends here ========== //
+
 // ===== mock potion test code goes here ===== //
 console.log(new Potion());
